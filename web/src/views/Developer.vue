ab704d6d4397dee5f5cb7a68f76a3adebea18515
Flowing <bitoreieru@gmail.com>
2020-03-10 11:27:15 +0900

    I just typo'd my name. fuck.

--- web/src/views/Developer.vue
+++ web/src/views/Developer.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
     <h1>Developer</h1>
-    <h2> Shugo <strong>FlowingSPDG</strong> Kwamura</h2>
+    <h2> Shugo <strong>FlowingSPDG</strong> Kawamura</h2>
+    <a href="https://github.com/FlowingSPDG/"> Github </a><br>
+    <a href="https://twitter.com/FlowingSPDG/"> @FlowingSPDG </a> / <a href="https://twitter.com/FlowingSPDG_EN/"> @FlowingSPDG_EN </a>
   </div>
 </template>
