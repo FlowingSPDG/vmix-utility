243915fdf18f6e30cbd7dc67bad47972fad03255
Flowing <30292185+ruk1ium@users.noreply.github.com>
2020-03-21 23:05:26 +0900

    Added double quotes besides my name

--- web/src/views/Developer.vue
+++ web/src/views/Developer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>Developer</h1>
-    <h2> Shugo <strong>FlowingSPDG</strong> Kawamura</h2>
+    <h2> Shugo "<strong>FlowingSPDG</strong>" Kawamura</h2>
     <a href="https://github.com/FlowingSPDG/"> Github </a><br>
     <a href="https://twitter.com/FlowingSPDG/"> @FlowingSPDG </a> / <a href="https://twitter.com/FlowingSPDG_EN/"> @FlowingSPDG_EN </a>
   </div>
