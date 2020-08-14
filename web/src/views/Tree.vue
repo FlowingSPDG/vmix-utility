63f8d666b166eab76290e3611681456d223f2ac0
Shugo Kawamura <shugo_k@flowing.tokyo>
2020-08-14 18:22:43 +0900

    Added refresh input button, Added "used by" section.

--- web/src/views/Tree.vue
+++ web/src/views/Tree.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="tree">
     <h1>Input Menu</h1>
+    <el-button round icon="el-icon-refresh-right" @click="Refresh">Refresh inputs</el-button>
     <el-table ref="singleTable" :default-sort = "{prop: 'Number', order: 'ascending'}" :data="inputs" style="width:85%;margin:auto;">
       <el-table-column label="Detail" type="expand">
         <template slot-scope="scope">
@@ -10,11 +11,18 @@
                 <el-table-column label="Name"><template slot-scope="scope"> {{SolveInputNameByKey(scope.row.Key)}}</template></el-table-column>
                 <el-table-column label="Key" prop="Key"></el-table-column>
             </el-table>
+
+            <h1>Used By...</h1>
+            <el-table ref="singleTable" :default-sort = "{prop: 'Number', order: 'ascending'}" :data="GetActiveUsedInputByKey(scope.row.Key)" style="width:85%;margin:auto;">
+                <el-table-column label="Index" prop="Index"><template slot-scope="scope"> {{SolveInputNumberByKey(scope.row.Key)}}</template></el-table-column>
+                <el-table-column label="Name"><template slot-scope="scope"> {{SolveInputNameByKey(scope.row.Key)}}</template></el-table-column>
+                <el-table-column label="Key" prop="Key"></el-table-column>
+            </el-table>
         </template>
       </el-table-column>
       <el-table-column label="Number" prop="Number"> </el-table-column>
       <el-table-column label="Type" prop="SceneType" sortable> </el-table-column>
-      <el-table-column label="Name" prop="Name" sortable> </el-table-column>
+      <el-table-column label="Title" prop="Title" sortable> </el-table-column>
       <el-table-column label="Key" prop="Key"></el-table-column>
     </el-table>
   </div>
@@ -25,7 +33,7 @@
 // @ is an alias to /src
 
 export default {
-  name: "Input Menu",
+  name: "InputMenu",
   components: {},
   data() {
     return {
@@ -47,6 +55,17 @@ export default {
           }
           return "NOT FOUND"
       },
+      SolveInputNumberByKey:function(key) {
+          if (!Array.isArray(this.inputs)) {
+              return
+          }
+          for (let i=0;i<this.inputs.length;i++) {
+              if (this.inputs[i].Key == key) {
+                  return this.inputs[i].Number
+              }
+          }
+          return 0
+      },
       SolveInputNameByNumber:function(num) {
           if (!Array.isArray(this.inputs)) {
               return
@@ -57,7 +76,39 @@ export default {
               }
           }
           return "NOT FOUND"
+      },
+      GetActiveUsedInputByKey:function(key){
+          if (!Array.isArray(this.inputs)) {
+              return []
+          }
+          const UsingInputs = []
+          for (let i=0;i<this.inputs.length;i++) {
+              if (Array.isArray(this.inputs[i].Overlay)) {
+                console.log(`overlay is array`)
+                 for (let o=0;o<this.inputs[i].Overlay.length;o++) {
+                    if (this.inputs[i].Overlay[o].Key == key) {
+                        UsingInputs.push(this.inputs[i])
+                    }
+              }
+              }
+          }
+          return UsingInputs
+      },
+      async Refresh(){
+        try{
+            this.inputs = await this.RefreshInput()
+            this.$notify({
+                title: "Success",
+                message: `Refreshed inputs.`,
+                type: "success"
+            });
+        }catch(err){
+            this.$notify.error({
+                title: "Error",
+                message: err
+        });
       }
+    }
   },
   watch:{
   }
