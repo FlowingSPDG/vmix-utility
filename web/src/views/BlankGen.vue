66f6174f409cff114e1e4507d0520326ab7d0c8c
Shugo Kawamura <shugo_k@flowing.tokyo>
2020-09-01 01:07:08 +0900

    Added blank generating page UI and API

--- web/src/views/BlankGen.vue
+++ web/src/views/BlankGen.vue
@@ -1,7 +1,54 @@
 <template>
   <div class="blank_gen">
     <h1>Blank Input Generator</h1>
-    <h2> Shugo "<strong>FlowingSPDG</strong>" Kawamura</h2>
-    
+    <el-checkbox v-model="transparent">Transparent</el-checkbox>
+    <br>
+    <el-input-number v-loading.fullscreen.lock="fullscreenLoading" v-model="num" :min="1" :max="999"></el-input-number>
+    <br>
+    <el-alert
+      center
+      title="大量のBlank生成はvMixへの過負荷、クラッシュを誘発する場合があります。"
+      type="warning">
+    </el-alert>
+    <el-alert
+      center
+      title="実際に生成されるBlankは指定した数より少なくなる場合があります。"
+      type="warning">
+    </el-alert>
+    <el-popconfirm @onConfirm="Send()" title="続行しますか？">
+      <el-button slot="reference">Confirm</el-button>
+    </el-popconfirm>
+
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      transparent: false,
+      num: 1,
+      fullscreenLoading: false
+    };
+  },
+  methods:{
+    async Send(){
+      this.fullscreenLoading = true
+      const queries = [{
+        "key":"Value",
+        "value":"Colour|Black"
+      }]
+      if (this.transparent) {
+        queries.value = "Colour|Transparent"
+      }
+      try{
+        return await this.SendsMultipleFunctions("AddInput", queries, this.num)
+      }catch(e){
+        // 
+      }finally{
+        this.fullscreenLoading = false
+      }
+    }
+  }
+}
+</script>
