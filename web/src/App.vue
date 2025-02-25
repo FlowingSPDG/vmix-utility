88fc8b949f4724b0500d9b0c288e3342930733b0
Shugo Kawamura <ruk1ium@gmail.com>
2025-02-25 20:28:46 +0900

    v1.18の軽微な修正 (#124)
    * Function候補からPreviewInputを削除
    
    * vMix Referenceを修正

--- web/src/App.vue
+++ web/src/App.vue
@@ -6,7 +6,7 @@
       <el-button round icon="el-icon-circle-plus" @click="JumpTo('/blank-gen')">Blank Generator</el-button>
       <el-button round icon="el-icon-user-solid" @click="JumpTo('/developer')">Developer</el-button>
       <el-button round icon="el-icon-s-grid" @click="moveLink('/multiviewer/index.html')">MultiView</el-button>
-      <el-button round icon="el-icon-link" @click="moveLink('https://www.vmix.com/help28/index.htm?ShortcutFunctionReference.html')">vMix Reference</el-button>
+      <el-button round icon="el-icon-link" @click="moveLink('https://www.vmix.com/help28/ShortcutFunctionReference.html')">vMix Reference</el-button>
     </div>
     <router-view/>
   </div>
